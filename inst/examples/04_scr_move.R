5fbb89c4560332087d8b2b544d8798afe02c97fc
richard <glennie@glennies.co.yk>
2019-07-26 15:24:39 +0100

    minor changes to scrmove exmp

--- inst/examples/04_scr_move.R
+++ inst/examples/04_scr_move.R
@@ -1,6 +1,6 @@
 # SCR transient model example 
 library(openpopscr)
-RcppParallel::setThreadOptions(numThreads = 3)
+RcppParallel::setThreadOptions(numThreads = 7)
 
 # simulate data -----------------------------------------------------------
 
@@ -21,7 +21,7 @@ scrdat <- simulate_scr(true_par, n_occasions, detectors, mesh, move = TRUE, seed
 
 # stationary fit ----------------------------------------------------------------
 
-stat <- ScrModel$new(list(lambda0 ~ 1, sigma ~ 1), 
+stat <- ScrModel$new(list(lambda0 ~ 1, sigma ~ 1, D ~ 1), 
                      scrdat, 
                      list(lambda0 = 0.5, sigma = 20, D = 1000))
 stat$fit()
@@ -31,7 +31,8 @@ stat
 
 form <- list(lambda0 ~ 1, 
              sigma  ~ 1, 
-             sd ~ 1)
+             sd ~ 1, 
+             D ~ 1)
 
 # get automatic start values 
 start <- get_start_values(scrdat, model = "ScrTransientModel")
@@ -40,6 +41,7 @@ start$lambda0 <- exp(stat$par()$lambda0[[1]])
 start$sigma <- exp(stat$par()$sigma[[1]])
 start$D <- exp(stat$par()$D[[1]])
 
+# create model object
 obj <- ScrTransientModel$new(form, scrdat, start)
 
 # compute initial likelihood 
