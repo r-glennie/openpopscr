9d16da0bf3ec523bae742eaebca146bcd75a6fe5
richard <glennie@glennies.co.uk>
2019-08-13 16:09:58 +0100

    makepar for jstrans

--- R/js_trans_model.R
+++ R/js_trans_model.R
@@ -31,15 +31,12 @@
 #'   \item print (default = TRUE): if TRUE then helpful output is printed to the screen
 #' }
 #' 
-#' Methods include: 
+#' Methods include those in JsModel with the following overwritten: 
 #' \itemize{
 #'  \item calc_initial_distribution(): computes initial distribution over life states (unborn, alive, dead)
 #'  \item calc_D_llk(): computes the likelihood of the D parameter
 #'  \item calc_pdet(): compute probability of being detected at least once during the survey
 #'  \item calc_llk(): compute log-likelihood at current parameter values 
-#'  \item fit(nsims = 99): fit the model by obtaining the maximum likelihood estimates. Estimates of
-#'        density are obtained from parametric boostrap with nsim resamples. 
-#'  \item simulate(): simulate ScrData object from fitted model
 #' }
 #' 
 JsTransientModel <- R6Class("JsTransientModel", 
@@ -47,12 +44,21 @@ JsTransientModel <- R6Class("JsTransientModel",
   public = list(
     
     initialize = function(form, data, start, detectfn = NULL, print = TRUE) {
+      private$check_input(form, data, start, detectfn, print)
       if (print) cat("Creating rectangular mesh......")
       newmesh <- rectangularMask(data$mesh())
       private$dx_ <- attr(newmesh, "spacing")
       private$inside_ <- as.numeric(pointsInPolygon(newmesh, data$mesh()))
       cov_list <- data$get_cov_list() 
-      private$data_ <- ScrData$new(data$capthist(), newmesh, data$time(), cov_list$cov, cov_list$cov_type, data$primary())
+      private$data_ <- ScrData$new(data$capthist(), newmesh, data$time(), data$primary())
+      covnms <- names(cov_list$cov)
+      allcovs <- names(private$data_$get_cov_list()$cov)
+      covnms <- covnms[!(covnms %in% allcovs)]
+      if (!is.null(covnms)) {
+        for (i in seq(covnms)) {
+          private$data_$add_covariate(covnms[i], cov_list$cov[[i]], cov_list$cov_type[i])
+        }
+      }
       box <- attributes(newmesh)$boundingbox
       region <- c(diff(box[1:2, 1]), diff(box[c(1, 3), 2]))
       private$num_cells_ <- numeric(3)
@@ -61,34 +67,19 @@ JsTransientModel <- R6Class("JsTransientModel",
       private$num_cells_[3] <- nrow(newmesh) / private$num_cells_[2]
       if (print) cat("done\n")
       if (print) cat("Reading formulae.......")
-      private$form_ <- form 
-      par_names <- sapply(form, function(f){f[[2]]})
-      if (!("D" %in% par_names)) {
-        private$form_ <- c(private$form_, list(D ~ 1))
-        par_names <- c(par_names, "D") 
-      }
-      # detection function 
-      if (is.null(detectfn)) {
-        private$detfn_ <- DetFn$new()
-      } else if (class(detectfn)[1] == "character") {
-        private$detfn_ <- DetFn$new(fn = detectfn)
-      } else {
-        private$detfn_ <- detectfn 
-      }
-      for (i in 1:private$detfn_$npars()) {
-        find <- par_names == private$detfn_$par(i)
-        if (all(!find)) stop("Parameters in formulae incorrect.")
-        private$form_[[i]]<- form[find][[1]]
-      }
-      private$form_[[private$detfn_$npars() + 1]] <- form[par_names == "phi"][[1]]
-      private$form_[[private$detfn_$npars() + 2]] <- form[par_names == "beta"][[1]]
-      private$form_[[private$detfn_$npars() + 3]] <- form[par_names == "sd"][[1]]
-      private$form_ <- lapply(private$form_, function(f) {delete.response(terms(f))})
+      order <- c("phi", "beta", "sd", "D")
+      private$read_formula(form, detectfn, order)
+      # add other parameters type
+      private$par_type_[private$detfn_$npars() + 1] <- "km"
+      private$par_type_[private$detfn_$npars() + 2] <- "k1m"
+      private$par_type_[private$detfn_$npars() + 3] <- "km"
+      private$par_type_[private$detfn_$npars() + 4] <- "m"
       names(private$form_) <- c(private$detfn_$pars(), "phi", "beta", "sd", "D")
+      # make parameter list 
       private$make_par() 
-      private$link2response_ <- c(private$detfn_$link2response(), list("plogis"), list("exp"), list("exp"), list("exp"))
+      private$link2response_ <- c(private$detfn_$link2response(), list("plogis"), list("mlogit"), list("exp"), list("exp"))
       names(private$link2response_) <- c(private$detfn_$pars(), "phi", "beta", "sd", "D")
-      private$response2link_ <- c(private$detfn_$response2link(), list("qlogis"), list("log"), list("log"), list("log"))
+      private$response2link_ <- c(private$detfn_$response2link(), list("qlogis"), list("invmlogit"), list("log"), list("log"))
       names(private$response2link_) <- c(private$detfn_$pars(), "phi", "beta", "sd", "D")
       if (print) cat("done\n")
       if (print) cat("Initialising parameters.......")
@@ -191,22 +182,8 @@ JsTransientModel <- R6Class("JsTransientModel",
       # compute log-likelihood
       llk <- llk - n * log(self$calc_Dpdet())
       llk <- llk + self$calc_D_llk()
-      #plot(self$par()$beta[-1])
       if(private$print_) cat("llk:", llk, "\n")
       return(llk)
-    },
-    
-    simulate = function(seed = NULL) {
-      if (!is.null(seed)) set.seed(seed)
-      new_dat <- simulate_js_openscr(self$par(), 
-                                     self$data()$n_occasions(), 
-                                     self$data()$traps(), 
-                                     self$data()$mesh(), 
-                                     move = TRUE, 
-                                     time = self$data()$time(), 
-                                     seed = seed, 
-                                     print = private$print_)
-      return(new_dat)
     }
 ),
                    
@@ -215,79 +192,24 @@ JsTransientModel <- R6Class("JsTransientModel",
     inside_ = NULL,
     num_cells_ = NULL,
     
-    make_par = function() {
-      samp_cov <- private$data_$covs(j = 1, k = 1, m = 1)
-      n_det_par <- private$detfn_$npars()
-      private$par_ <- vector(mode = "list", length = n_det_par + 1)
-      n_par <- numeric(n_det_par + 4)
-      for (par in 1:(n_det_par + 4)) {
-        X <- model.matrix(private$form_[[par]], data = samp_cov)
-        n_par[par] <- ncol(X)
-        if (par %in% c(n_det_par + 1, n_det_par + 2) & "t" %in% all.vars(private$form_[[par]])) {
-          n_par[par] <- ncol(X) - 1
-          par_vec <- rep(0, n_par[par])
-          names(par_vec) <- colnames(X)[colnames(X) != paste("t", private$data_$n_occasions() - 1, sep ="")]
-        } else if (par %in% c(n_det_par + 1, n_det_par + 2) & "primary" %in% all.vars(private$form_[[par]])) {
-          n_par[par] <- ncol(X) - 1
-          par_vec <- rep(0, n_par[par])
-          names(par_vec) <- colnames(X)[colnames(X) != paste("primary", private$data_$n_occasions() - 1, sep ="")]
-        } else {
-          n_par[par] <- ncol(X)
-          par_vec <- rep(0, n_par[par])
-          names(par_vec) <- colnames(X)
-        }
-        private$par_[[par]] <- par_vec
-      }
-      names(private$par_) <- names(private$form_) 
-      return(invisible())
-    }, 
-    
     initialise_par = function(start) {
       n_det_par <- private$detfn_$npars()
       names <- private$detfn_$pars()
       for (i in 1:n_det_par) {
-        private$par_[[i]][1] <- do.call(private$response2link_[[i]], 
-                                        list(start[[i]]))
+        private$par_[[names[i]]][1] <- do.call(private$response2link_[[names[i]]], 
+                                               list(start[[names[i]]]))
       }
       private$par_$phi[1] <- do.call(private$response2link_$phi, 
                                            list(start$phi))
       private$par_$beta[1] <- do.call(private$response2link_$beta,
-                                        list(c(1 / start$beta - 1)))
+                                      list(c(start$beta, rep((1 - start$beta) / (self$data()$n_occasions() - 1), self$data()$n_occasions() - 1))))[1]
       private$par_$sd[1] <-do.call(private$response2link_$sd, 
                                      list(start$sd))
-      if (length(private$par_$beta) > 1) {
-        private$par_$beta <- rep(1 / start$beta - 1, private$data_$n_occasions() - 1)
-        private$par_$beta <- private$par_$beta / length(private$par_$beta)
-        private$par_$beta <- do.call(private$response2link_$beta, 
-                                         list(private$par_$beta))
-        private$par_$beta[-1] <- private$par_$beta[-1] - private$par_$beta[1]
-      }
       private$par_$D[1] <- do.call(private$response2link_$D, 
                                          list(start$D / private$data_$n_meshpts()))
+      # compute initial parameters for each jkm
+      private$compute_par()
       return(invisible())
-    }, 
-    
-    convert_vec2par = function(vec) {
-      par <- NULL
-      n_occasions <- private$data_$n_occasions()
-      names <- names(vec)
-      n_det_par <- self$detectfn()$npars()
-      parnames <- self$detectfn()$pars()
-      par <- vector(mode = "list", length = n_det_par)
-      for (i in 1:n_det_par) {
-        par[[i]] <- vec[grep(parnames[i], names)]
-        names(par[[i]]) <- gsub(paste0(parnames[i],"."), "", names(par[[i]]))
-      }
-      names(par) <- parnames 
-      par$phi <- vec[grep("phi", names)]
-      names(par$phi) <- gsub("phi.", "", names(par$phi))
-      par$beta <- vec[grep("beta", names)]
-      names(par$beta) <- gsub("beta.", "", names(par$beta))
-      par$sd <- vec[grep("sd", names)]
-      names(par$sd) <- gsub("sd.", "", names(par$sd))
-      par$D <- vec[grep("D", names)]
-      names(par$D) <- gsub("D", "", names(par$D))
-      return(par)
     }
   )                 
 )
