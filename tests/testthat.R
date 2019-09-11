2857e784a4735694e59f8eb930d9d7399b510ea4
richard <glennie@glennies.co.uk>
2019-09-11 10:49:26 +0100

    filter did not work

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,5 +1,3 @@
 ## TESTTHAT 
 library(testthat)
-test_check("openpopscr", filter = "scr")
-test_check("openpopscr", filter = "cjs")
-test_check("openpopscr", filter = "js")
+test_check("openpopscr")
